75887505dbbd4ffb68473863b6317d8564a12962
juan1445 <juanzuleta1445@gmail.com>
2019-09-24 16:45:09 -0500

    Home terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ class Footer extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <footer className="footer mt-auto py-3 bg-light">
+                <footer className="footer bg-light">
                     <div className="container text-center ">
                         <div className="row">
                             <div className="col-4 col-md-4">
-                            <i className="fas fa-road color-orange fa-2x"></i>
+                            <i className="fas fa-road color-orange"></i>
                             <p>Trayectos</p>
                             </div>
                             <div className="col-4 col-md-4">
-                            <i className="fas fa-home color-orange fa-2x"></i>
+                            <i className="fas fa-home color-orange"></i>
                             <p>Inicio</p>
                             </div>
                             <div className="col-4 col-md-4">
-                            <i className="fas fa-paper-plane color-orange fa-2x"></i>
+                            <i className="fas fa-paper-plane color-orange"></i>
                             <p>Pqrs</p>
                             </div>
                         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,8 @@ class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <main>
+        <main className="mb-5">
+
           <div className="bg-color-home">
             <div className="position-relative overflow-hidden p-3 p-md-5 text-center bg-home">
               <div className="col-md-5 p-lg-5 mx-auto my-5 bg-color-home">
@@ -23,10 +24,60 @@ class Home extends React.Component {
               </div>
             </div>
           </div>
+
+          
+          <div className=" py-5">
+      <div className="container">
+
+        <div className="row mb-5">
+          <div className="col-md-3 ml-auto">
+            <h2>¿ Que hacemos ?</h2>
+          </div>
+          <div className="col-md-5 mr-auto">
+            <p className="lead text-dark">
+              Somos una app de movilidad en donde podrás realizar tus <strong>QUEJAS</strong> o <strong>SUGERENCIAS</strong> sobre el medio de transporte en el que te movilizas
+            </p>
+          </div>
+        </div>
+
+        <div className="row mt-5">
+          <div className="col-md-6">
+            <div className="media">
+              <div className="icon mr-3">
+                <i className="fas fa-paper-plane fa-4x text-success"></i>
+              </div>
+              <div className="media-body">
+                <h3 className="h4">Sugerencias</h3>
+                <p class="text-dark text-left">
+                  Puedes realizar sugerencias para los medios de transporte que consideras que servirian para mejorar la movilidad en Medellín.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="media">
+              <div className="icon mr-3">
+                <i className="fas fa-exclamation-circle text-danger fa-4x"></i>
+              </div>
+              <div className="media-body">
+                <h3 className="h4">Quejas</h3>
+                <p className="text-dark text-left">
+                  Si tienes alguna queja sobre el medio de transporte que utilizas este es el mejor lugar para hacerla, haremos llegar todas tus quejas a las entidades encargadas.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr/>
+
+
           <div className="container mt-5">
             <div className="row">
-              <div className="col-12 col-md-6">
-                <h2 className="text-center mb-4">Últimas noticias</h2>
+              <div className="col-12 col-md-6 mb-4">
+                <h2 className="text-center mb-5">Últimas noticias</h2>
                 <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                   <div className="col p-4 d-flex flex-column position-static">
                     <strong className="d-inline-block mb-2 text-primary">
@@ -54,8 +105,8 @@ class Home extends React.Component {
                     <h3 className="mb-0">Estación Acevedo</h3>
                     <div className="mb-1 text-muted">Sep 18</div>
                     <p className="card-text mb-auto">
-                      Se han instalado mas de 20 buses nuevos en la estación acevedo
-                      que llevaran a distintos lugares de la ciudad 
+                      Se han instalado mas de 20 buses nuevos en la estación
+                      acevedo que llevaran a distintos lugares de la ciudad
                     </p>
                     <a href="/" className="stretched-link">
                       Continuar leyendo
@@ -65,9 +116,13 @@ class Home extends React.Component {
                     <img src={metro} width="220px" height="260px" alt="" />
                   </div>
                 </div>
+                <div className="text-right">
+                <button className="btn color-orange bg-orange text-white bnt-lg">Ver más</button>
+                </div>
               </div>
-              <div className="col-12 col-md-6">
-                  <h2 className="text-center">Nuestro Twitter</h2>
+              <div className="col-12 col-md-6 text-center">
+                <h2 className="text-center mb-5">Nuestro Twitter</h2>
+                <p>Aquí van los twitters de MovilApp</p>
               </div>
             </div>
           </div>
