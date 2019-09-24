280034e517a3efeea8c89194cadd646c710f29ab
juan1445 <juanzuleta1445@gmail.com>
2019-09-24 11:52:29 -0500

    Terminado el Splash

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import Logo from "./images/logo.jpg";
+import "./styles/splash.css";
 
 class Splash extends React.Component {
   render() {
-    return <h1>Hola mundo</h1>;
+    return (
+      <React.Fragment>
+        <div className="container text-center center">
+          <div className="mb-5">
+            <h1>MovilApp</h1>
+          </div>
+          <div className="mb-5">
+            <img
+              src={Logo}
+              alt="Logo"
+              className="borde"
+              width="300"
+              height="250"
+            />
+          </div>
+          <div className="mb-5">
+            <h4>Por una mejor movilidad</h4>
+          </div>
+        </div>
+        <div className="footer text-center text-secondary">
+          <h6>
+            <small>MovilApp 2019. Todos los derechos reservados.</small>
+          </h6>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
