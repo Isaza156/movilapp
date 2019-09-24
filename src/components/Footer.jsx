a042f94b93f97bd701f943f6cbe7b671edb51155
juan1445 <juanzuleta1445@gmail.com>
2019-09-24 17:19:42 -0500

    act

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,15 @@ class Footer extends React.Component{
                 <footer className="footer bg-white border-top">                
                     <div className="container text-center ">
                         <div className="row">
-                            <div className="col-4 col-md-4 pt-2">
+                            <div className="col-4 col-md-4 pt-3">
                             <i className="fas fa-road color-orange"></i>
                             <p>Trayectos</p>
                             </div>
-                            <div className="col-4 col-md-4 pt-2">
+                            <div className="col-4 col-md-4 pt-3">
                             <i className="fas fa-home color-orange"></i>
                             <p>Inicio</p>
                             </div>
-                            <div className="col-4 col-md-4 pt-2">
+                            <div className="col-4 col-md-4 pt-3">
                             <i className="fas fa-paper-plane color-orange"></i>
                             <p>Pqrs</p>
                             </div>
