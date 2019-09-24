f895cb814dd045e8961e3e1eeaf6c9004e10a5bf
juan1445 <juanzuleta1445@gmail.com>
2019-09-24 12:59:29 -0500

    Footer Organizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,19 @@ class Footer extends React.Component{
         return(
             <React.Fragment>
                 <footer className="footer mt-auto py-3 bg-light fixed-bottom">
-                    <div className="container justify-content-center ">
+                    <div className="container text-center ">
                         <div className="row">
                             <div className="col-4 col-md-4">
                             <i className="fas fa-road color-orange fa-2x"></i>
+                            <p>Trayectos</p>
                             </div>
                             <div className="col-4 col-md-4">
-                            <i className="fas fa-plus-circle color-orange fa-3x"></i>
+                            <i className="fas fa-home color-orange fa-2x"></i>
+                            <p>Inicio</p>
                             </div>
                             <div className="col-4 col-md-4">
                             <i className="fas fa-paper-plane color-orange fa-2x"></i>
+                            <p>Pqrs</p>
                             </div>
                         </div>
                     </div>
