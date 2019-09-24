d7e4aa113fbdf904e34f3498a8591695d244ca9c
santiagohenao97 <santiago.henao@hotmail.com>
2019-09-24 13:10:34 -0500

    login

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Layout from "./components/Layout";
 import Splash from "./components/Splash";
-
+import Login from './components/Login';
 import NotFound from "./pages/NotFound";
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/" component={Splash} />>
+          <Route exact path="/" component={Login} />>
           <Route component={NotFound} />
+          
         </Switch>
       </Layout>
     </BrowserRouter>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-dom";
+import Logo from '../components/images/logo.png'
+
+class Login extends React.Component {
+  render() {
+    
+  }
+}
+
+export default Login;
