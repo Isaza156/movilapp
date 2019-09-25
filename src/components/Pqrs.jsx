1391015615398c768a20b2b776bc53844326cab2
oela <oela@azulBook.local>
2019-09-24 21:51:12 -0500

    consumiuendo api

--- src/components/Pqrs.jsx
+++ src/components/Pqrs.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Card from './Card_pqrs'
 import CallToAction from "./CallToAction";
 
 class Pqrs extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+    (async () => {
+      try {
+        let data = await fetch('http://localhost:3000/publicacion/');
+        data = await data.json()
+        // const data = ["data"]
+        console.log(data);
+
+        this.setState({ data })
+      } catch (error) {
+        console.error(error);
+      }
+
+    })()
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -12,33 +35,20 @@ class Pqrs extends React.Component {
           <li className="fas fa-chevron-right fa-2x color-orange"></li>
         </div>
         <div className="container mb-5">
-          <div className="card">
+
+          {
+            !this.state.data ?
+              "loading feed..."
+              :
+              this.state.data.map(entry => <Card data={entry} />)
+          }
+
+          {/* <div className="card mt-3">
             <div className="card-body">
-                <div className="d-flex justify-content-between">
-                <small className="form-text text-muted">ALCALDÍA/TRÁNSITO</small>
-                <small className="form-text text-muted">1 min</small>
-                </div>
-              <h5 className="card-title">Buseteros compitiendo</h5>
-              <p className="card-text">
-                Los buseteros de bello compiten por recoger pasajeros y están poniendo en riesgo la vida de nosotros.
-                <span className="text-info">#buseteros#busesbello#bello#masrespetoconlospasajeros</span>
-              </p>
-              <div className="d-flex justify-content-between pt-3">
-                <div>
-                <i className="fas fa-heart text-danger fa-2x"></i>
-                </div>
-                <div><a href="/" className="btn color-orange border">
-                Comentar
-              </a></div>
-              </div>
-            </div>
-          </div>
-          <div className="card mt-3">
-            <div className="card-body">
-                <div className="d-flex justify-content-between">
+              <div className="d-flex justify-content-between">
                 <small className="form-text text-muted">METRO</small>
                 <small className="form-text text-muted">5 min</small>
-                </div>
+              </div>
               <h5 className="card-title">El metro hace muchas paradas</h5>
               <p className="card-text">
                 Normalmente un viaje de san antonio a la estrella demora 25 minutos, pero la mayoría de veces esta demorando mas de 40 minutos porque para antes de llegar a cada estación.
@@ -46,20 +56,20 @@ class Pqrs extends React.Component {
               </p>
               <div className="d-flex justify-content-between pt-3">
                 <div>
-                <i className="fas fa-heart text-danger fa-2x"></i>
+                  <i className="fas fa-heart text-danger fa-2x"></i>
                 </div>
                 <div><a href="/" className="btn color-orange border">
-                Comentar
+                  Comentar
               </a></div>
               </div>
             </div>
           </div>
           <div className="card mt-3">
             <div className="card-body">
-                <div className="d-flex justify-content-between">
+              <div className="d-flex justify-content-between">
                 <small className="form-text text-muted">TRÁNSITO/VÍAS</small>
                 <small className="form-text text-muted">15 min</small>
-                </div>
+              </div>
               <h5 className="card-title">Accidentes por huecos</h5>
               <p className="card-text">
                 Se ha vuelto costumbre ver accidentes en la vía robledo por huecos, muy peligrosas esas calles
@@ -67,14 +77,14 @@ class Pqrs extends React.Component {
               </p>
               <div className="d-flex justify-content-between pt-3">
                 <div>
-                <i className="fas fa-heart text-danger fa-2x"></i>
+                  <i className="fas fa-heart text-danger fa-2x"></i>
                 </div>
                 <div><a href="/" className="btn color-orange border">
-                Comentar
+                  Comentar
               </a></div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
         <CallToAction />
       </React.Fragment>
