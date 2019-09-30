636cc87ae267357a60a72a58da6d529b1ef582d1
juan1445 <juanzuleta1445@gmail.com>
2019-09-29 22:40:57 -0500

    listas las Faqs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Layout from "./components/Layout";
-import Splash from "./components/Splash";
-
 import NotFound from "./pages/NotFound";
+import Faqs from "./components/Faqs";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/" component={Splash} />>
+          <Route exact path="/" component={Faqs} />>
           <Route component={NotFound} />
         </Switch>
       </Layout>
