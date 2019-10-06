5ef5230d063df66afb3bbbf19fc96487da2eef49
cold-briu <andresavena19@gmail.com>
2019-10-06 14:28:43 -0500

    esto no está funcionando, toca buscar docs, tal vez una librería

--- src/services/userAuth.service.js
+++ src/services/userAuth.service.js
@@ -1,12 +1,39 @@
 function userAuth() {
 
-    let userIsAuth = true
+    let userIsAuth = false
+    let userId = ""
+    let userData = ""
 
     this.getAuth = function () { // publicly available method that a bird object can use
         return userIsAuth;
     };
-    this.setAuth = function () {
-        userIsAuth = true
+
+    this.getUserData = function () { // publicly available method that a bird object can use
+        return userId;
+    };
+
+    this.setAuth = async function (data) {
+        try {
+            const url = 'https://aka-geek.appspot.com/api/usuarios/login'
+            const response = await fetch(url, {
+                method: 'POST',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data),
+            });
+
+            let authData = await response.json()
+
+            if (response.status == 400) return authData.authRes
+
+            userIsAuth = true
+            userId = authData
+            return userIsAuth
+
+        } catch (err) {
+            console.error(err)
+        }
     }
 }
 
