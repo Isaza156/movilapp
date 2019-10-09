58922554255512e287a11e31fb645a5154507ed5
juan1445 <juanzuleta1445@gmail.com>
2019-10-09 13:12:46 -0500

    archivo move actualizado

--- src/pages/Move.jsx
+++ src/pages/Move.jsx
@@ -3,7 +3,54 @@ import WOW from "wowjs";
 import { Link } from "react-router-dom";
 import "../components/styles/move.css";
 
+  
 class Move extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      caminando:false,
+      bicicleta:false,
+      metro:false,
+      taxi:false,
+      plataformas:false,
+      bus:false,
+      moto:false,
+      carro:false,
+      otro:false,
+      descripcion:"",
+      check1:false,
+      check2:false
+    }
+    this.onCheckChange = this.onCheckChange.bind(this)
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+  }
+  onCheckChange(e){
+    console.log(e.target.checked);
+    this.setState({
+        [e.target.name]: e.target.checked
+
+    })
+  }
+  // handleChange = e => {
+  //   this.setState({
+  //     values: {
+  //       ...this.state.values,
+  //       [e.target.name]: e.target.value
+  //     }
+  //   });
+  // };
+
+
+
+//   handleSubmit(e){
+//   e.preventDefault();
+//    console.log(this.state.values.caminando)
+//  }
+
+
   componentDidMount() {
     new WOW.WOW({
       live: false
@@ -12,7 +59,8 @@ class Move extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="container mt-5 wow fadeIn">
+      <form onSubmit={this.handleSubmit}> 
+        <div className="container pt-5 mt-5 wow fadeIn">
           <h2 className="mb-5 text-center">¿Cómo me movilizo?</h2>
           <div className="container row ml-2">
             <div className="col-6">
@@ -20,11 +68,18 @@ class Move extends React.Component {
                 <input
                   type="checkbox"
                   className="custom-control-input"
-                  id="customControlAutosizing1"
+                  id="caminando"
+                  name="caminando"
+                  checked={this.state.caminando}
+                  onChange={this.onCheckChange}
+                 // value={this.state.values.caminando}
+
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing1"
+                  htmlFor="caminando"
+                  name="caminando"
+                  //value={this.state.values.caminando}
                 >
                   Caminando
                 </label>
@@ -33,11 +88,15 @@ class Move extends React.Component {
                 <input
                   type="checkbox"
                   className="custom-control-input"
-                  id="customControlAutosizing2"
+                  id="bicicleta"
+                  name="bicicleta"
+                  checked={this.state.bicicleta}
+                  onChange={this.onCheckChange}
+         //         value={this.state.values.bicicleta}
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing2"
+                  htmlFor="bicicleta"
                 >
                   Bicicleta
                 </label>
@@ -46,11 +105,15 @@ class Move extends React.Component {
                 <input
                   type="checkbox"
                   className="custom-control-input"
-                  id="customControlAutosizing3"
+                  id="metro"
+                  name="metro"
+                  checked={this.state.metro}
+                  onChange={this.onCheckChange}
+           //       value={this.state.values.metro}
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing3"
+                  htmlFor="metro"
                 >
                   Metro
                 </label>
@@ -59,39 +122,51 @@ class Move extends React.Component {
                 <input
                   type="checkbox"
                   className="custom-control-input"
-                  id="customControlAutosizing4"
+                  id="taxi"
+                  name="taxi"
+                  checked={this.state.taxi}
+                  onChange={this.onCheckChange}
+             //     value={this.state.values.taxi}
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing4"
+                  htmlFor="taxi"
                 >
                   Taxi
                 </label>
               </div>
-              <div className="custom-control custom-checkbox mr-sm-2 mb-3">
+              {/* <div className="custom-control custom-checkbox mr-sm-2 mb-3">
                 <input
                   type="checkbox"
                   className="custom-control-input"
-                  id="customControlAutosizing5"
+                  id="plataformas"
+                  name="plataformas"
+                  checked={this.state.plataformas}
+                  onChange={this.onCheckChange}
+               //   value={this.state.values.plataformas}
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing5"
+                  htmlFor="plataformas"
                 >
                   Plataformas
                 </label>
-              </div>
-            </div>
+              </div>*/}
+            </div> 
             <div className="col-6">
               <div className="custom-control custom-checkbox mr-sm-2 mb-3">
                 <input
                   type="checkbox"
                   className="custom-control-input"
-                  id="customControlAutosizing6"
+                  id="bus"
+                  name="bus"
+                  checked={this.state.bus}
+                  onChange={this.onCheckChange}
+                 // value={this.state.values.bus}
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing6"
+                  htmlFor="bus"
                 >
                   Bus
                 </label>
@@ -100,11 +175,15 @@ class Move extends React.Component {
                 <input
                   type="checkbox"
                   className="custom-control-input text-danger"
-                  id="customControlAutosizing7"
+                  id="moto"
+                  name="moto"
+                  checked={this.state.moto}
+                  onChange={this.onCheckChange}
+                  //value={this.state.values.moto}
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing7"
+                  htmlFor="moto"
                 >
                   Moto
                 </label>
@@ -113,11 +192,15 @@ class Move extends React.Component {
                 <input
                   type="checkbox"
                   className="custom-control-input"
-                  id="customControlAutosizing8"
+                  id="carro"
+                  name="carro"
+                  checked={this.state.carro}
+                  onChange={this.onCheckChange}
+                  //value={this.state.values.carro}
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing8"
+                  htmlFor="carro"
                 >
                   Carro
                 </label>
@@ -126,11 +209,15 @@ class Move extends React.Component {
                 <input
                   type="checkbox"
                   className="custom-control-input"
-                  id="customControlAutosizing9"
+                  id="otro"
+                  name="otro"
+                  checked={this.state.otro}
+                  onChange={this.onCheckChange}
+             //     value={this.state.values.otro}
                 />
                 <label
                   className="custom-control-label"
-                  htmlFor="customControlAutosizing9"
+                  htmlFor="otro"
                 >
                   Otro
                 </label>
@@ -138,6 +225,7 @@ class Move extends React.Component {
               <input
                 className="form-control line"
                 type="text"
+                name="descripcion"
                 placeholder="¿Cuál?"
               />
             </div>
@@ -147,7 +235,7 @@ class Move extends React.Component {
             <div className="custom-control custom-radio mr-sm-2 mb-3">
               <input
                 type="radio"
-                name="check"
+                name="check1" //medio de transporte sitio / estudio
                 className="custom-control-input"
                 id="customControlAutosizing10"
               />
@@ -162,7 +250,7 @@ class Move extends React.Component {
             <div className="custom-control custom-radio mr-sm-2 mb-3">
               <input
                 type="radio"
-                name="check"
+                name="check2" //medio de transporte como herramienta de trabajo
                 className="custom-control-input"
                 id="customControlAutosizing11"
               />
@@ -209,13 +297,19 @@ class Move extends React.Component {
             </div>
           </div>
           <div className="container text-center">
-            <Link to="/home">
-              <button type="button" className="btn btn-lg mb-5 btn-login">
+            {/* <Link to="/home"> */}
+              <button 
+               type="button"
+               className="btn btn-lg mb-5 btn-login"
+               onClick={this.handleClick}
+               >
                 Iniciar
               </button>
-            </Link>
+            {/* </Link> */}
           </div>
         </div>
+      </form>
+     
       </React.Fragment>
     );
   }
