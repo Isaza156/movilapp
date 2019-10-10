1c04a444104bd8274ac10944fdfcb87557665fb6
juan1445 <juanzuleta1445@gmail.com>
2019-10-10 08:53:59 -0500

    act

---
+++ src/assets/utils/mocks/athleteMocks.js
@@ -0,0 +1,186 @@
+const athletes = [
+  {
+    id: "9c5e57ca-97a9-4e5f-80ec-dca103e5eb20",
+    name: "Giorgi",
+    last_name: "Iggo",
+    document: 10,
+    city: "Kashima-shi",
+    sport: "natacion",
+    email: "giggo0@sina.com.cn"
+  },
+  {
+    id: "2c0df2be-e638-4325-b744-9bd9cf998eac",
+    name: "Tonya",
+    last_name: "Fairest",
+    document: 10,
+    city: "Côte-Saint-Luc",
+    sport: "natacion",
+    email: "tfairest1@51.la"
+  },
+  {
+    id: "38d33e7e-46c1-4f48-a51e-8d360757805c",
+    name: "Jilly",
+    last_name: "Caskie",
+    document: 10,
+    city: "Ylitornio",
+    sport: "atletismo",
+    email: "jcaskie2@wiley.com"
+  },
+  {
+    id: "10494e87-61bd-4ffa-a856-12ad42c1a29f",
+    name: "Franchot",
+    last_name: "Buncombe",
+    document: 10,
+    city: "Calidñgan",
+    sport: "atletismo",
+    email: "fbuncombe3@theguardian.com"
+  },
+  {
+    id: "bd693d36-c69e-4641-80d7-c53d95266edf",
+    name: "Charlot",
+    last_name: "Kunrad",
+    document: 10,
+    city: "Matala",
+    sport: "baloncesto",
+    email: "ckunrad4@engadget.com"
+  },
+  {
+    id: "8df50549-540f-4aa0-ab9d-f16eaad59195",
+    name: "Cori",
+    last_name: "Raun",
+    document: 10,
+    city: "Pedro Juan Caballero",
+    sport: "atletismo",
+    email: "craun5@eepurl.com"
+  },
+  {
+    id: "4cda7858-01fc-43b6-abdf-bbefe554cb0f",
+    name: "Lorraine",
+    last_name: "d'Escoffier",
+    document: 10,
+    city: "Krolevets’",
+    sport: "atletismo",
+    email: "ldescoffier6@storify.com"
+  },
+  {
+    id: "cdbad08f-517a-43aa-aa52-6ace9ad53519",
+    name: "Merill",
+    last_name: "Brasier",
+    document: 10,
+    city: "Gucheng",
+    sport: "baloncesto",
+    email: "mbrasier7@goo.ne.jp"
+  },
+  {
+    id: "eda555e2-2a1f-453d-9678-68cfa78cb7d2",
+    name: "Dev",
+    last_name: "Charrett",
+    document: 10,
+    city: "Sernur",
+    sport: "natacion",
+    email: "dcharrett8@paypal.com"
+  },
+  {
+    id: "f8edbbcd-0676-4226-8ce6-bd70942ef092",
+    name: "Arabella",
+    last_name: "Friskey",
+    document: 10,
+    city: "Jiangti",
+    sport: "baloncesto",
+    email: "afriskey9@alexa.com"
+  },
+  {
+    id: "4e4bb174-3cb9-4d77-8dcd-7b9dde646074",
+    name: "Glory",
+    last_name: "Raincin",
+    document: 10,
+    city: "Tha Ruea",
+    sport: "baloncesto",
+    email: "graincina@desdev.cn"
+  },
+  {
+    id: "9472fa01-f631-4d08-a85a-0983c36f53d9",
+    name: "Koren",
+    last_name: "Surman",
+    document: 10,
+    city: "Setono",
+    sport: "atletismo",
+    email: "ksurmanb@usnews.com"
+  },
+  {
+    id: "59f51e53-7b47-426f-bdd0-0eea2313db1a",
+    name: "Roderic",
+    last_name: "Lambillion",
+    document: 10,
+    city: "Seupakat",
+    sport: "futbol",
+    email: "rlambillionc@nba.com"
+  },
+  {
+    id: "4dcecf8e-46f2-46ff-9972-79e49378184c",
+    name: "Damian",
+    last_name: "Holwell",
+    document: 10,
+    city: "Bordeaux",
+    sport: "natacion",
+    email: "dholwelld@skype.com"
+  },
+  {
+    id: "23b289e8-0d19-45a9-907d-1151c4fbdf1c",
+    name: "Ardra",
+    last_name: "Nore",
+    document: 10,
+    city: "Bayan",
+    sport: "natacion",
+    email: "anoree@blog.com"
+  },
+  {
+    id: "cdeaac54-a432-4175-a71a-56ad81d9eece",
+    name: "Caresa",
+    last_name: "Heigold",
+    document: 10,
+    city: "Novi Itebej",
+    sport: "futbol",
+    email: "cheigoldf@unesco.org"
+  },
+  {
+    id: "8f268357-b141-44ed-bb5d-6f9636ff720b",
+    name: "Perla",
+    last_name: "Clynmans",
+    document: 10,
+    city: "Casal dos Bernardos",
+    sport: "futbol",
+    email: "pclynmansg@skyrock.com"
+  },
+  {
+    id: "09b04aff-7d51-4e08-aba8-1b0d9baefbe6",
+    name: "Ericka",
+    last_name: "Clift",
+    document: 10,
+    city: "Washington",
+    sport: "atletismo",
+    email: "eclifth@webs.com"
+  },
+  {
+    id: "b2432478-de2c-461d-b039-f7138b25f81c",
+    name: "Sophronia",
+    last_name: "Vidgen",
+    document: 10,
+    city: "Dongluqiao",
+    sport: "voleibol",
+    email: "svidgeni@opensource.org"
+  },
+  {
+    id: "77232d74-7144-4b42-9b18-5098a70e6c2b",
+    name: "Angelika",
+    last_name: "Samet",
+    document: 10,
+    city: "Przecław",
+    sport: "futbol",
+    email: "asametj@blog.com"
+  }
+];
+
+module.exports = {
+  athletes
+};
---
+++ src/assets/utils/mocks/specialtyMocks.js
@@ -0,0 +1,41 @@
+const specialty = [
+  {
+    id: "41c30328-7478-43bb-b884-49dcfdd8aed2",
+    user: "phue0",
+    password: "PW0kFyP6",
+    email: "eabbots0@arstechnica.com",
+    " specialty": "enfermeria"
+  },
+  {
+    id: "780da731-f7bc-486e-8197-aa33d036c754",
+    user: "lduesberry1",
+    password: "LBow86lOO7",
+    email: "aodo1@npr.org",
+    " specialty": "nutricion"
+  },
+  {
+    id: "5f487bc8-7ffd-4209-bb50-272ff62d231c",
+    user: "mjurisch2",
+    password: "UFuY9yCrue",
+    email: "klilian2@ning.com",
+    " specialty": "enfermeria"
+  },
+  {
+    id: "5c33f883-ae8b-4602-b832-d58bb17cb531",
+    user: "dlates3",
+    password: "xmwREvPm",
+    email: "fstodit3@howstuffworks.com",
+    " specialty": "cineantropometria"
+  },
+  {
+    id: "1a8d61cb-954c-442f-bf7c-1c4a4026f9bf",
+    user: "smeates4",
+    password: "33ReH4o6",
+    email: "cchellenham4@theguardian.com",
+    " specialty": "odontologia"
+  }
+];
+
+module.exports = {
+  specialty
+};
